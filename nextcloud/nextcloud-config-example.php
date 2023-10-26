5e562c8d2a3d0063a8efdd8c82d069d738355a7d
jcookin <>
2023-10-25 22:52:01 -0600

    Add sample config file lines to enable redis in config file

---
+++ nextcloud/nextcloud-config-example.php
@@ -0,0 +1,13 @@
+//Add these lines to the end of the file (before the close `);` )
+// file: server_files/config/config.php
+
+<?php
+
+'maintenance' => false,
+   'memcache.local' => '\\OC\\Memcache\\Redis',
+   'redis' =>
+   array (
+     'host' => 'redis',
+     'port' => 6379,
+   ),
+   'memcache.locking' => '\\OC\\Memcache\\Redis',
